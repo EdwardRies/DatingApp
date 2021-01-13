23b02c638b3106800d88473328cbd82d9f87e774
Edward Ries (CloudFit Software) <v-edrie@microsoft.com>
2021-01-13 13:54:10 -0500

    added account service, nav, home, register components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -9,29 +9,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'The Dating App';
-  users: any;
-  count: number;
 
-  constructor(private http: HttpClient) {
-    this.count = 0;
+  constructor(private accountService: AccountService) {
+
   }
   
   ngOnInit(): void {
-    this.getUsers();
-    this.clock();
+    this.setCurrentUser();
   }
   
-  getUsers() { 
-    this.http.get('https://localhost:5001/api/users')
-    .subscribe(response => { this.users = response; }, error => { console.log(error); });
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
   
-  clock() { 
-    setInterval(callBack => { this.clockIncrement() }, 1000);
-  }
-
-  clockIncrement(): number {
-    return this.count++;
-  }
-
 }
